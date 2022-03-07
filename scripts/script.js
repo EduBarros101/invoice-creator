3dc099cf8a3e8c2b36d9e28cf903e1053d001dea
Eduardo Barros <edu.barros101@gmail.com>
2022-03-07 13:55:22 -0300

    Structuring the app

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
+// =========================================================
+
+// For the sake of testing and training, I had it done on JavaScript but, after talking to some people in the industrie, it seems that it should feel like an "over do" what I should simply accomplish on pure HTML and CSS.
+
 // function invoiceCard() {
 //     const cardBody = document.createElement('div')
 //     cardBody.classList.add('card')
@@ -11,3 +15,5 @@
 // }
 
 // document.body.onload = invoiceCard()
+
+// =========================================================
