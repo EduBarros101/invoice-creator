9165a13bdeb69e97e3ca72b2e9785ccf9f2f3448
Eduardo Barros <edu.barros101@gmail.com>
2022-03-17 12:05:11 -0300

    Names and Values criated

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,29 @@
-// Buttons
 const button1 = document.querySelector('#button1')
 const button2 = document.querySelector('#button2')
 const button3 = document.querySelector('#button3')
 
-const button1Value = 10
-const button2Value = 20
-const button3Value = 30
+const taskBarTH1 = document.querySelector('#tasks-ordered-bar :nth-child(1)')
+const taskBarTH2 = document.querySelector('#tasks-ordered-bar :nth-child(2)')
 
-button1.textContent = `Wash Car: $${button1Value}`
-button2.textContent = `Mow Lawn: $${button2Value}`
-button3.textContent = `Pull Weeds: $${button3Value}`
+const ordersList = document.querySelector('#tasks-ordered-list')
+
+let ordersArray = []
+
+let button1Name = 'Wash Car'
+let button2Name = 'Mow Lawn'
+let button3Name = 'Pull Weeds'
+
+let button1Value = 10
+let button2Value = 20
+let button3Value = 30
+
+taskBarTH1.textContent = 'TASK'
+taskBarTH2.textContent = 'TOTAL'
+
+button1.textContent = `${button1Name}: $${button1Value}`
+button2.textContent = `${button2Name}: $${button2Value}`
+button3.textContent = `${button3Name}: $${button3Value}`
+
+button1.addEventListener('click', () => {
+    // TODO: Objects?
+})
